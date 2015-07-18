c1504963cdd35220e1452675330dc69ef139435b
maddyonline <madhavjha@gmail.com>
2015-07-18 00:42:15 -0700

    adding save feature

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({ model: function() {
-	return this.modelFor('friends/show').get('articles'); }
+export default Ember.Route.extend({
+	model: function() {
+		return this.modelFor('friends/show').get('articles');
+	},
+	actions: {
+		save: function(model) {
+			model.save();
+			return false;
+		}
+	}
 });
-
